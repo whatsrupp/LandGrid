f60ce063ad4f2cfdf7c2055d5b86849d32ba350b
whatsrupp <nickrupp95@gmail.com>
2019-10-19 18:06:51 +0100

    :sparkles: :white_check_mark: add percentile calculations and test

--- server/routes/data/__tests__/parseData.spec.js
+++ server/routes/data/__tests__/parseData.spec.js
@@ -2,23 +2,23 @@ const parseData = require('../parseData')
 const path = require('path');
 
 describe("dataParser()", () => {
-    it("extracts x coordinate, y coordinate and price into an array of objects from a text file", async () => {
+    it("extracts x coordinate, y coordinate and price into an array of objects from a text file and calculates the percentile", async () => {
         const filePath = path.resolve(__dirname, 'data.test.txt');
 
         const expected = [{
             x: 1,
             y: 2,
-            price: 3
+            price: 3,
+            percentile: 50,
         }, {
             x: 4,
             y: 5,
             price: 6,
+            percentile: 100,
         }]
 
         const result = await parseData(filePath);
 
         expect(result).toEqual(expected)
-
-        
     });
 });
--- server/routes/data/parseData.js
+++ server/routes/data/parseData.js
@@ -7,17 +7,30 @@ const parseData = async (filePath) => {
 
     const data = await readFile(filePath)
     const  rows = data.toString().split("\n");
+
+    let maxPrice = 0;
     const parsedData = rows.reduce((acc, row)=>{
         const columns = row.split(' ')
-        const [x, y, price] = columns
+        const [x, y, price] = columns;
+        
+        if(parseInt(price) > maxPrice){
+            maxPrice = price;
+        }
+
         acc.push({x: parseInt(x), y: parseInt(y), price: parseInt(price)})
+
         return acc
     }, [])
-    
 
+    const enhancedData = parsedData.map((row)=>{
 
-    return parsedData
-
+        const percentile = (row.price/maxPrice) * 100;
+        return({
+            ...row,
+            percentile
+        })
+    })
+    return enhancedData
 }
 
 module.exports = parseData
