eca590db97a87bcbcddec3e658caa086a14129e1
whatsrupp <nickrupp95@gmail.com>
2019-10-19 18:07:35 +0100

    :sparkles: add data parser into api route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const path = require('path')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('hello world')
+const parseData = require('./data/parseData')
+
+router.get('/', async (req, res) => {
+
+  const dataFile =  path.resolve(__dirname, 'data/data.txt');
+  try{
+    const data = await parseData(dataFile)
+    res.send(data)
+
+  }catch(err){
+    res.send(err)
+  }  
+
 });
 
 module.exports = router;
