ec86a42813040d4745cfb3290464ddac0a130e93
whatsrupp <nickrupp95@gmail.com>
2019-10-20 15:15:01 +0100

    :sparkles: implement cors

--- server/app.js
+++ server/app.js
@@ -3,12 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors');
 var indexRouter = require('./routes/index');
 
 var app = express();
 
-// view engine setup
+app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(logger('dev'));
@@ -19,13 +19,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
